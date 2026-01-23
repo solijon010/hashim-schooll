566c481c3b1e01eea36921766078d33019a35adb
kohhorov09 <your-email@example.com>
2026-01-23 20:15:41 +0500

    Logout qlish funcsiyasi qo'shildi

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCAXfTwBQyViKTDsP3OSO4K3N8DC7p5oGI",
@@ -16,4 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
 export const storage = getStorage(app);
